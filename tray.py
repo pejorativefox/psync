cae854b8d3e5ae1cc8f81284b8ec68669da6d048
Chris Statzer <chris.statzer@gmail.com>
2026-05-07 14:54:13 +0800

    MMoved to pyside6

--- tray.py
+++ tray.py
@@ -1,21 +1,18 @@
-import sys
-
-from PyQt5 import QtWidgets, QtGui
+from PySide6 import QtWidgets, QtGui
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
         menu = QtWidgets.QMenu(parent)
         exit_action = menu.addAction("Exit")
-        exit_action.triggered.connect(QtWidgets.qApp.quit)
+        exit_action.triggered.connect(QtWidgets.QApplication.instance().quit)
         self.setContextMenu(menu)
 
 def main(image):
     app = QtWidgets.QApplication([])
-    w = QtWidgets.QWidget()
-    tray_icon = SystemTrayIcon(QtGui.QIcon(image), w)
+    tray_icon = SystemTrayIcon(QtGui.QIcon(image))
     tray_icon.show()
-    sys.exit(app.exec_())
+    app.exec_()
 
 if __name__ == '__main__':
-    main('./assets/idle.png')
+    main('assets/idle.png')
