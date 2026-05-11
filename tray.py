dbffc12c49c835cf07c97cfc559642e94c0f1bc5
Chris Statzer <chris.statzer@gmail.com>
2026-05-11 08:46:27 +0800

    basic app flow

--- tray.py
+++ tray.py
@@ -1,4 +1,15 @@
+import sys
 from PySide6 import QtWidgets, QtGui
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MainWindow")
+        self.setCentralWidget(QtWidgets.QLabel("MainWindow"))
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
@@ -8,11 +19,27 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         exit_action.triggered.connect(QtWidgets.QApplication.instance().quit)
         self.setContextMenu(menu)
 
+class PsyncApp(QtWidgets.QApplication):
+    def __init__(self, args, icon_path):
+        super().__init__(args)
+        self.setQuitOnLastWindowClosed(False)
+        
+        self.window = MainWindow()
+        self.tray_icon = SystemTrayIcon(QtGui.QIcon(icon_path))
+        self.tray_icon.activated.connect(self.on_tray_activated)
+        self.tray_icon.show()
+
+    def on_tray_activated(self, reason):
+        if reason == QtWidgets.QSystemTrayIcon.ActivationReason.Trigger:
+            if self.window.isVisible():
+                self.window.hide()
+            else:
+                self.window.show()
+                self.window.activateWindow()
+
 def main(image):
-    app = QtWidgets.QApplication([])
-    tray_icon = SystemTrayIcon(QtGui.QIcon(image))
-    tray_icon.show()
-    app.exec_()
+    app = PsyncApp(sys.argv, image)
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
     main('assets/idle.png')
